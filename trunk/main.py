fc701d92d8e9138f0e61c6f32890ce79d074dac3
archwyrm <archwyrm@20d81b1c-a155-0410-a04c-b3564f34cd2f>
2008-06-12 04:10:50 +0000

    Some bare bones files

---
+++ trunk/main.py
@@ -0,0 +1,8 @@
+# The main loop for this
+
+print 'Welcome to some lord2-like game!'
+
+def main():
+    print 'Game screen'
+
+main()
---
+++ trunk/src/Map.py
@@ -0,0 +1,4 @@
+class Map:
+    """Holds the data describing one map screen, i.e.: all the terrain,
+    hotspots, etc.."""
+    pass
