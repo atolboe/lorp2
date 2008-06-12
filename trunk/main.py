d0687d1b173f14563d9266204fdca0f4c98b3015
archwyrm <archwyrm@20d81b1c-a155-0410-a04c-b3564f34cd2f>
2008-06-12 07:24:13 +0000

    Added map loading and some more skeleton stuff.

--- trunk/main.py
+++ trunk/main.py
@@ -1,8 +1,16 @@
 # The main loop for this
 
+import sys
+sys.path.append('src')
+
+import Map
+
 print 'Welcome to some lord2-like game!'
 
 def main():
-    print 'Game screen'
+    map = 'map.dat'
+    print 'Loading map from', map
+    scr = Map.Screen()
+    scr.load_file(map)
 
 main()
